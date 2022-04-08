9b2a16950ac8288d49a37c1a50050591d5df0bf5
Viet281101 <nva281101@gmail.com>
2022-04-08 03:13:15 +0200

    project

--- Script/Characters/states/combat/Defense.gd
+++ Script/Characters/states/combat/Defense.gd
@@ -2,4 +2,11 @@ extends Motion
 
 
 func _ready():
-	pass
+	$ShieldPivot.hide()
+	Global.connect("direction_changed", self, '_on_Parent_direction_changed')
+
+func _physics_process(delta):
+	$ShieldPivot.global_position = owner.global_position
+
+func _on_Parent_direction_changed(direction):
+	$ShieldPivot.rotation = direction.angle()
